5b2c0c326334a3da514754d702d0089c35e52bb1
Rojalin Achary <102635042+writecodewithme@users.noreply.github.com>
2022-09-29 13:34:16 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+let formel=document.getElementById("form-section")
+console.log(formel)
+function button(){
+    formel.innerHTML+="<p>THANK YOU FOR SUBMITTING</p>"
+    // window.location.href="index.html"
+
+}
